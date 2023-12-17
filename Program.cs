ff0f5613191c348d4c941c5262a2b26a6e023c35
user <user@DESKTOP-A74NBNA>
2023-12-17 18:29:20 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1. Дано целое число n, указывающее номер дня недели от 1 до 7.
+            //По указанному номеру n вывести название соответствующего дня недели.
+
+
+            //Console.Write("n = ");
+            //int n = int.Parse(Console.ReadLine());
+
+            //switch (n)
+            //{
+            //    case 1: Console.WriteLine("понедельник");break;
+            //    case 2: Console.WriteLine("вторник");break; 
+            //    case 3: Console.WriteLine("среда");break;
+            //    case 4: Console.WriteLine("четверг");break;
+            //    case 5: Console.WriteLine("пятница");break;
+            //    case 6: Console.WriteLine("суббота");break;
+            //    case 7: Console.WriteLine("воскресенье");break;
+            //}
+
+
+            //Задача 2.В зависимости от порядкового номера месяца(1, 2, ... , 12)
+            //вывести на экран его название(январь, февраль, ... , декабрь).
+
+
+            //Console.Write("n = ");
+            //int n = int.Parse(Console.ReadLine());
+
+            //switch (n)
+            //{
+            //    case 1: Console.WriteLine("январь"); break;
+            //    case 2: Console.WriteLine("февраль"); break;
+            //    case 3: Console.WriteLine("март"); break;
+            //    case 4: Console.WriteLine("апрель"); break;
+            //    case 5: Console.WriteLine("май"); break;
+            //    case 6: Console.WriteLine("июнь"); break;
+            //    case 7: Console.WriteLine("июль"); break;
+            //    case 8: Console.WriteLine("август"); break;
+            //    case 9: Console.WriteLine("сентябрь"); break;
+            //    case 10: Console.WriteLine("октрябрь"); break;
+            //    case 11: Console.WriteLine("ноябрь"); break;
+            //    case 12: Console.WriteLine("декабрь"); break;
+            //}
+
+            //Задача 3.По номеру месяца вывести название времени года.
+
+            //Console.Write("n = ");
+            //int n = int.Parse(Console.ReadLine());
+
+            //switch (n)
+            //{
+            //    case 12: case 1: case 2: Console.WriteLine("зима"); break;
+            //    case 3: case 4: case 5: Console.WriteLine("весна"); break;
+            //    case 7: case 8: case 6: Console.WriteLine("лето"); break;
+            //    case 10: case 11: case 9: Console.WriteLine("осень"); break;
+            //}
+
+
+            //Задача 4.Вывести название предмета по введенной первой букве:
+            //ф – физика, м – математика, и – история, г – география, б – биология.
+
+
+            //Console.Write("n = ");
+            //string n = Console.ReadLine();
+
+            //switch (n)
+            //{
+            //    case "ф": Console.WriteLine("физика"); break;
+            //    case "м": Console.WriteLine("математика"); break;
+            //    case "и": Console.WriteLine("история"); break;
+            //    case "г": Console.WriteLine("география((("); break;
+            //    case "б": Console.WriteLine("биология"); break;
+            //}
+
+            Console.Read();
+        }
+    }
+}
